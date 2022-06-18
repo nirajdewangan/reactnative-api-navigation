1b0c712fd675bd868d85c353acf48a98361145f9
Sunny Dhanwani <sunny.dhanwani@joinleadoff.com>
2022-06-19 00:54:01 +0530

    Changed to expo

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+//all screens
+import Home from './screens/Home';
+import Add from './screens/Add';
+import Edit from './screens/Edit';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} options={{
+          headerStyle: {
+            backgroundColor: "#0f4c75"
+          },
+          title: 'My Doctor App',
+          headerTitleStyle: {
+            textAlign: "center",
+            color: "#00b7c2"
+          }
+        }} />
+        <Stack.Screen name="Add" component={Add} options={{
+          headerStyle: {
+            backgroundColor: "#0f4c75"
+          },
+          title: 'My Doctor App',
+          headerTitleStyle: {
+            textAlign: "center",
+            color: "#00b7c2"
+          }
+        }} />
+        <Stack.Screen name="Edit" component={Edit} options={{
+          headerStyle: {
+            backgroundColor: "#0f4c75"
+          },
+          title: 'My Doctor App',
+          headerTitleStyle: {
+            textAlign: "center",
+            color: "#00b7c2"
+          }
+        }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/Edit.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Text, StyleSheet } from 'react-native'
+
+
+const  Edit = () => {
+    return(
+        <Text>Edit </Text>
+    )
+}
+
+export default Edit;
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#1b262c',
+      flex: 1,
+      justifyContent: 'flex-start',
+    },
+    heading: {
+      textAlign: 'center',
+      color: '#00b7c2',
+      marginHorizontal: 5,
+      marginTop: 50,
+      marginBottom: 20,
+    },
+    formItem: {
+      marginBottom: 20,
+    },
+  });
